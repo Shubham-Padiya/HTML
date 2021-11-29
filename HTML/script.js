c53a8d4e82c1f16c460bef52641a87da868ab4f7
Shubham-Padiya <95012298+Shubham-Padiya@users.noreply.github.com>
2021-11-29 22:56:35 +0530

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,74 @@
+let rotation = 0;
+function rotateimg1() {
+  rotation += 90;
+  if (rotation === 180) {
+    rotation = 0;
+  }
+      document.querySelector("#img1").style.transform = `rotate(${rotation}deg)`;
+      }
+
+
+function rotateimg2() {
+  rotation += 90; 
+  if (rotation === 180) {
+    rotation = 0;
+  }
+      document.querySelector("#img2").style.transform = `rotate(${rotation}deg)`;
+      }
+
+
+function rotateimg3() {
+  rotation += 90; 
+  if (rotation === 180) {
+    rotation = 0;
+}
+    document.querySelector("#img3").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg4() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img4").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg5() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img5").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg6() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img6").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg7() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img7").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg8() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img8").style.transform = `rotate(${rotation}deg)`;
+}
+
+function rotateimg9() {
+    rotation += 90; 
+    if (rotation === 180) {
+      rotation = 0;
+  }
+      document.querySelector("#img9").style.transform = `rotate(${rotation}deg)`;
+}
